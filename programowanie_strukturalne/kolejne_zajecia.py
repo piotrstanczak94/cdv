5536e22dbd8b80be90d7b05c8956532a6f3d4de3
piotrstanczak94 <piotr.andrzej.stanczak@gmail.com>
2019-05-12 14:53:28 +0200

    Add new files to Git

---
+++ programowanie_strukturalne/kolejne_zajecia.py
@@ -0,0 +1,43 @@
+x = 5
+if x == 5:
+    print('x jest równe 5')
+    print(f'x = {x}')
+else:
+    print(f'x jest równe {x}')
+
+print('Za instrukcją warunkową')
+
+#######################
+
+y = True        #False "z wielkiej litery"
+
+if y:
+    print('Prawda')
+else:
+    print('Fałsz')
+
+#########################
+
+z = 5 > 6
+if z:
+    print('p')
+else:
+    print('f')
+
+##########################
+
+j = '1'
+
+if bool(j):
+    print('1')
+else:
+    print('0')
+
+############################
+
+k = input('Podaj k: ')
+if bool(k):
+    print('Zmienna k zawiera dane')
+else:
+    print("Zmienna k nie zawiera danych")
+    
